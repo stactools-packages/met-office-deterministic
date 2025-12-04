5d8517599c9efecf30bb615abb937bcd55d81cd0
Pete Gadomski <pete.gadomski@gmail.com>
2025-12-04 07:46:22 -0700

    feat: add more asset descriptions (#25)

--- tests/test_stac.py
+++ tests/test_stac.py
@@ -1,6 +1,7 @@
 import datetime
 
 import pytest
+from pystac import Item
 
 from stactools.met_office_deterministic import stac
 from stactools.met_office_deterministic.constants import Model, Theme
@@ -14,11 +15,24 @@ from stactools.met_office_deterministic.constants import Model, Theme
 def test_collection(model: Model, theme: Theme) -> None:
     collection = stac.create_collection(model, theme)
     assert collection.title
+    assert len(collection.links) == 2
+
+    license_link = next(link for link in collection.links if link.rel == "license")
+    assert license_link.href == "https://creativecommons.org/licenses/by-sa/4.0/deed.en"
+    assert license_link.media_type == "text/html"
+
+    describedy_link = next(
+        link for link in collection.links if link.rel == "describedby"
+    )
+    assert (
+        describedy_link.href
+        == "https://www.metoffice.gov.uk/services/data/external-data-channels"
+    )
+
     collection.validate()
 
 
-def test_items(hrefs: list[str]) -> None:
-    items = stac.create_items(hrefs)
+def test_items(items: list[Item]) -> None:
     for item in items:
         datetime.datetime.strptime(
             item.properties["forecast:reference_datetime"], "%Y-%m-%dT%H:%M:%SZ"
@@ -29,4 +43,15 @@ def test_items(hrefs: list[str]) -> None:
             assert asset.media_type == "application/netcdf", "No media type: " + key
             assert asset.title
 
+            if should_have_descriptions(item):
+                assert asset.description
+
         item.validate()
+
+
+def should_have_descriptions(item: Item) -> bool:
+    # Temporary helper function while we wait for more descriptions from the met office
+    model = item.properties["met_office_deterministic:model"]
+    return model == Model.uk or item.properties["met_office_deterministic:theme"] in [
+        Theme.near_surface,
+    ]
