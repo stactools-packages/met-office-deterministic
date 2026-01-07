26359f57b973e0895c009620b759c4a575393cb6
Pete Gadomski <pete.gadomski@gmail.com>
2026-01-07 06:05:06 -0700

    test: add regression test for height_of_orography (#39)

--- tests/test_href.py
+++ tests/test_href.py
@@ -5,3 +5,10 @@ def test_hrefs(hrefs: list[str]) -> None:
     for raw_href in hrefs:
         href = Href.parse(raw_href)
         href.item_id.startswith("20250614T")
+
+
+def test_no_variable() -> None:
+    href = Href.parse(
+        "http://stactools-met-office-deterministic.test/global-deterministic-10km/20250614T0000Z/20250614T0000Z-PT0000H00M-height_of_orography.nc"
+    )
+    assert href.variable is None
